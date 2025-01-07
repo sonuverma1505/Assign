b7033a0c7f6f1ed66600fc69726aebac84dd69d3
sonuverma1505 <34466195+sonuverma1505@users.noreply.github.com>
2025-01-08 02:43:06 +0530

    Update app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 80;
 
-// Read the secret from the environment variable
-const helloWorldSecret = process.env.HELLO_WORLD_SECRET || 'Secret not found';
+// Path to the file where the secret is mounted
+const secretFilePath = '/vault/secrets/clisecret';
+
+let helloWorldSecret = 'Secret not found';
+
+// Read the secret directly from the file
+try {
+  helloWorldSecret = fs.readFileSync(secretFilePath, 'utf8').trim(); // Read and trim the content
+} catch (err) {
+  console.error(`Error reading secret file: ${err.message}`);
+}
 
 app.get('/', (req, res) => {
   res.send(`Hello World! This is the output: ${helloWorldSecret}`);
